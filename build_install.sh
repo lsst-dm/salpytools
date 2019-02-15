981aaf362c2461b86afe292a8f8018f260ba810b
Felipe Menanteau <felipe@illinois.edu>
2019-02-15 12:50:20 -0600

    Add install and setup.py scripts
    Update to install scripts
    
    Update install location
    
    Added setpath.sh file to setup.py

---
+++ build_install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Config values
+VERSION="tickets/DM-16255" # This is the git tag or branch
+PRODUCT=salpytools
+GIT_LSST="https://github.com/lsst-dm"
+#INSTALL_PATH=/usr/local
+#INSTALL_PATH=$HOME/test-install
+INSTALL_PATH=/opt
+
+# Change accordingly
+PRODUCT_DIR=$INSTALL_PATH/$PRODUCT/$VERSION
+INSTALL_DIR=$INSTALL_PATH/$PRODUCT/$VERSION/sandbox-install
+
+# Git clone and version checkout
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+git clone $GIT_LSST/$PRODUCT.git
+cd $PRODUCT
+git checkout $VERSION
+
+# Build and install
+echo "# INSTALL_DIR: "$INSTALL_DIR
+echo "# PRODUCT_DIR: "$PRODUCT_DIR
+mkdir -p $PRODUCT_DIR
+export PYTHONPATH=$PYTHONPATH:$PRODUCT_DIR/python
+python setup.py install --prefix=$PRODUCT_DIR --install-lib=$PRODUCT_DIR/python
+
+# Clean up
+echo "# Cleaning up:" $INSTALL_DIR
+rm -rf $INSTALL_DIR
---
+++ setup.py
@@ -0,0 +1,19 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*")
+data_files = [("",["setpath.sh"])]
+
+# The main call
+setup(name='salpytools',
+      version ='OCS_IntSpecICC',
+      license = "GPL",
+      description = "Python tools to connect to SAL.",
+      author = "LSST, Felipe Menanteau",
+      author_email = "felipe@illinois.edu",
+      packages = ['salpytools'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=data_files,
+      )
