38491bf8266c486ff35a87c13c574e1f56e117fd
Felipe Menanteau <felipe@illinois.edu>
2019-05-16 11:51:36 -0500

    Adding Tag to setup.py and __init__ files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ data_files = [("",["setpath.sh"])]
 
 # The main call
 setup(name='salpytools',
-      version ='0.9.3',
+      version ='0.9.4',
       license = "GPL",
       description = "Python tools to connect to SAL.",
       author = "LSST, Felipe Menanteau",
